0b9782996a6e53a8c64c8b518b0f826000b5311e
Scott Jenkins <sjenkins94@gmail.com>
2021-08-05 16:53:06 +0900

    Added roxygen skeleton to tpose, removed fbind

--- R/tpose.R
+++ R/tpose.R
@@ -1,3 +1,14 @@
+#' Transpose a data.frame preserving names
+#'
+#'Takes in a data.frame and returns it transposed, with the original row names as column names and the original column names as row names.
+#'
+#' @param df data.frame
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples
+#' tpose(mtcars[1:5, 1:5])
 tpose <- function(df){
   t_df <- data.table::transpose(df)
   colnames(t_df) <- rownames(df)
